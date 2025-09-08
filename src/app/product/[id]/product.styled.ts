5774e6be108436baca9446c92e7f0d5b61f01be1
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-09-08 15:06:45 -0300

    styles(product): Ajustar estilos finais do produto

--- src/app/product/[id]/product.styled.ts
+++ src/app/product/[id]/product.styled.ts
@@ -15,7 +15,7 @@ export const StylesProduct = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-    padding: 40px;
+    padding: 20px;
   }
 
   .content {
