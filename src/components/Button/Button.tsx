9e371e4720c4147e71d93ba67536ddf81f8773ca
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-09-08 14:24:03 -0300

    fix(Button) remover marcadores de merge do Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 // Não utilizei esse componente no projeto, mas pode ser inserido futuramente!
-=======
-// Não utilizei esse componente no projeto, mas pode ser inserido futuramente!
->>>>>>> 538853da316d080275206b612f5cc8cba249a16f
